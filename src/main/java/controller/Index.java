a3d260d3fd2ca5961d72b44531c16811120f271b
antoinepryy <antoine.ap.57@gmail.com>
2019-05-06 12:53:57 +0200

    router test ok

--- src/main/java/controller/Index.java
+++ src/main/java/controller/Index.java
@@ -6,14 +6,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet(name = "Home")
-public class Home extends HttpServlet {
+@WebServlet(name = "Index")
+public class Index extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/home.jsp").forward(request, response);
     }
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
